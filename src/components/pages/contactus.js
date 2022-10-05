4c9c7579fb8fb82a0177e7f72d41e08dfc9758c6
Jarod Brown <j.marise.b@gmail.com>
2022-10-05 19:52:26 -0400

    tailwind int

--- src/components/pages/contactus.js
+++ src/components/pages/contactus.js
@@ -2,5 +2,5 @@ import React from "react";
 import '../../App.css'
 
 export default function ContactUs(){
-    return <h1 className='contactus'>Contact Us</h1>;
+    return <h1 className='bg-red-700'>Contact Us</h1>;
 }
