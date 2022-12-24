4f5e29644ed5c6cb067817da4581f4321ba30923
SaiRahul Kilambi <boogltd@gmail.com>
2022-12-23 21:13:53 -0500

    yessir

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -17,7 +17,7 @@ function CardItem(props){
 <Link className="cards_item_link" to={props.path}>
             <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
             <figure className="cards_item_pic-wrap" data-category={props.label}>
-        <img className="rounded-t-lg cards_item_img" src={props.src} alt="Event" />
+        <img className="rounded-t-lg cards_item_img" src={props.src}  alt="Event" />
         </figure>
     
     <div className="p-5 cards_item_info">
